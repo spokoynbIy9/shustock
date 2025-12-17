2fb1328177eafe19224ddec6fe4cf91e3af174b9
Gusarov Anatoly <gusarov.tolia@mail.ru>
2025-12-17 19:17:11 +0500

    fix: добавить поля totalCount, total в cart-slice

--- store/slices/cart/cart-slice.ts
+++ store/slices/cart/cart-slice.ts
@@ -4,10 +4,14 @@ import { Good } from "@/types";
 
 interface CartState {
     goods: Good[];
+    totalCount: number;
+    total: number;
 }
 
 const initialState: CartState = {
     goods: [],
+    totalCount: 0,
+    total: 0,
 };
 
 export const cartSlice = createSlice({
@@ -16,18 +20,33 @@ export const cartSlice = createSlice({
     reducers: {
         addGood: (state, action: PayloadAction<Good>) => {
             state.goods.push(action.payload);
+            state.totalCount += 1;
+            state.total += action.payload.price;
         },
         deleteGood: (state, action: PayloadAction<string>) => {
+            const goodToDelete = state.goods.find(
+                (good) => good.id === action.payload,
+            );
+
+            if (goodToDelete) {
+                state.total -= goodToDelete.price;
+                state.totalCount -= 1;
+            }
+
             state.goods = state.goods.filter(
                 (good) => good.id !== action.payload,
             );
         },
         deleteAllGoods: (state) => {
             state.goods = [];
+            state.total = 0;
+            state.totalCount = 0;
         },
     },
     selectors: {
         getGoods: (state) => state.goods,
+        getTotal: (state) => state.total,
+        getTotalCount: (state) => state.totalCount,
     },
 });
 
