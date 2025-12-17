b11944fc5781c50b00f812304406406deaf5b92e
Gusarov Anatoly <gusarov.tolia@mail.ru>
2025-12-17 19:15:17 +0500

    fix: изменить поле authorName на authorId

--- types/good.ts
+++ types/good.ts
@@ -3,7 +3,7 @@ import { Author } from "./author";
 export interface Good {
     id: string;
     title: string;
-    authorName: Author["id"];
+    authorId: Author["id"];
     price: number;
     image: string;
 }
